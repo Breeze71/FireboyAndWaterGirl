ebc036f179d1a109107caee2c1462b7c364f56d7
Chris_Zeng <j92080412@gmail.com>
2023-11-18 21:28:23 +0800

    Play Through

--- Assets/_FireboyAndWaterGirl/Scripts/Interactable/DoorInteraction.cs
+++ Assets/_FireboyAndWaterGirl/Scripts/Interactable/DoorInteraction.cs
@@ -6,12 +6,12 @@ public class DoorInteraction : InteractableBase
 {
     public override void EnterTrigger(Collider2D _other)
     {
-        
+        GameEventManager.Instance.playerEvent.EnterDoor();
     }
 
     public override void ExitTrigger(Collider2D _other)
     {
-        
+        GameEventManager.Instance.playerEvent.ExitDoor();
     }
 
     public override void Interact()
